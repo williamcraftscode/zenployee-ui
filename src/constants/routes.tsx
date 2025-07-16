b572b15caa3f6d196dfb537f9ae45d15745a7fdc
William <williamlangroudi@gmail.com>
2025-07-16 12:00:17 +0200

    Add routes

---
+++ src/constants/routes.tsx
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  DASHBOARD: "/",
+  EMPLOYEES: "/employees",
+  ADD_EMPLOYEE: "/employees/add",
+  EDIT_EMPLOYEE: "/employees/edit/:id",
+  VIEW_EMPLOYEE: "/employees/view/:id",
+} as const;
